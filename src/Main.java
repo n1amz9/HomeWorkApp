cda948cba806c00bae39d24eecd96f1fe09531dc
n1amz9 <derpy.h00ves@mail.ru>
2022-02-28 16:55:49 +0300

    homework #1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,59 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        printThreeWords();      //  Задание №1
+        EndOfCase();    //  окончание задания
+        checkSumSign();         //  Задание №2
+        EndOfCase();    //  окончание задания
+        printColor();           //  Задание №3
+        compareNumbers();       //  Задание №4
+        EndOfCase();    //  окончание задания
+        EndOfCase();    //  окончание задания
+
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = -2;
+        int b = -3;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = -5;
+        if (value > 0 && value < 100) {
+            System.out.println("Желтый");
+        }
+        else if (value > 100) {
+            System.out.println("Зеленый");
+        }
+        else {
+            System.out.println("Красный");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 5;
+        int b = 5;
+        if (a >= b)
+        {
+            System.out.println(" a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
+    public static void EndOfCase() {
+        System.out.println("======= Конец задания  =======");
     }
 }
