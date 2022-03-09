baca0ea929d243a1fb9218e95e415fce6a898bff
n1amz9 <derpy.h00ves@mail.ru>
2022-03-09 21:01:55 +0300

    homework #3 (неполная)

---
+++ src/Homework_3/Arrays.java
@@ -0,0 +1,91 @@
+package Homework_3;
+
+import java.sql.Array;
+
+public class Arrays {
+    public static void main(String[] args) {
+        task_1();
+        task_2();
+        task_3();
+        System.out.println("===");
+        task_4();
+    }
+
+    //1. Задать целочисленный массив, состоящий из элементов 0 и 1.
+    // Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+    public static void task_1() {
+        int[] MyArray = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < MyArray.length; i++) {
+            switch (MyArray[i]) {
+                case 1:
+                    MyArray[i] = 0;
+                    break;
+                case 0:
+                    MyArray[i] = 1;
+                    break;
+            }
+        }
+        System.out.println(java.util.Arrays.toString(MyArray));
+    }
+
+    //2. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
+    public static void task_2() {
+        int[] MyArray = new int[100];
+        for (int i = 0; i < 100; i++) {
+            MyArray[i] = i + 1;
+        }
+        System.out.println(java.util.Arrays.toString(MyArray));
+    }
+
+    //3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+    public static void task_3() {
+        int[] MyArray = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < MyArray.length; i++) {
+            if (MyArray[i] < 6) {
+                MyArray[i] *= 2;
+            }
+        }
+        System.out.println(java.util.Arrays.toString(MyArray));
+    }
+
+    //4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью цикла(-ов) заполнить его диагональные элементы единицами
+// (можно только одну из диагоналей, если обе сложно). Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны,
+// то есть [0][0], [1][1], [2][2], …, [n][n];
+    public static void task_4() {
+        int[][] MySuperArray;
+        MySuperArray = new int[10][10];
+        for (int i = 0; i < MySuperArray.length; i++) {
+            for (int j = 0; j < MySuperArray[0].length; j++) {
+                if (i == j) {
+                    MySuperArray[i][j] = 1;
+                }
+                if (i + j == MySuperArray.length - 1) {
+                    MySuperArray[i][j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < MySuperArray.length; i++) {
+            {
+                System.out.println(java.util.Arrays.toString(MySuperArray[i]));
+            }
+        }
+    }
+
+    //5. Написать метод, принимающий на вход два аргумента: len и initialValue,
+    // и возвращающий одномерный массив типа int длиной len, каждая ячейка которого равна initialValue;
+    public static void task_5(int len, int initialValue) {
+
+    }
+
+}
+
+
+
+//6. * Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
+
+//7. ** Написать метод,
+// в который передается не пустой одномерный целочисленный массив, метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны.
+
+// 8. *** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или отрицательным), при этом метод должен сместить все элементы массива на n позиций.
+// Элементы смещаются циклично. Для усложнения задачи нельзя пользоваться вспомогательными массивами.
+
