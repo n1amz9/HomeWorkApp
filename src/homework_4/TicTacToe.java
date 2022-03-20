de1deef054e101cad486b9bdb16001e660b54b56
n1amz9 <derpy.h00ves@mail.ru>
2022-03-20 21:33:47 +0300

    homework #4 (in progress..) - пока не робит, вылетает с ошибкой. Разбираюсь

--- src/homework_4/TicTacToe.java
+++ src/homework_4/TicTacToe.java
@@ -1,19 +1,22 @@
 package homework_4;
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TicTacToe {
     public static char[][] map;
-    public static final int SIZE = 5;
+    public static final int SIZE = 3;
     public static final int DOT_TO_WIN = 3;
     public static final char DOT_EMPTY = '-';
-    public static final char DOT_x = 'X';
+    public static final char DOT_X = 'X';
     public static final char DOT_O = 'O';
-
+    public static Scanner scanner = new Scanner("Введите координаты в формате X Y");
 
     public static void main(String[] args) {
         initMap();
         printMap();
+        humanTurn();
     }
 
     public static void initMap() {
@@ -25,24 +28,40 @@ public class TicTacToe {
         }
     }
 
-    public static void printMap()
-    {
-        for (int i = 0; i <= SIZE; i++) {
-            if (i == 0) {
+    public static void printMap() {
+        for (int i = -1; i < SIZE; i++) {
+            if (i == -1) {
                 System.out.print("  ");
-            }
-            else {
+            } else {
                 System.out.print(i + " ");
             }
         }
         System.out.println();
         for (int i = 0; i < SIZE; i++) {
-            System.out.print((i + 1) + " ");
+            System.out.print((i) + " ");
             for (int j = 0; j < SIZE; j++) {
                 System.out.print(map[i][j] + " ");
             }
             System.out.println();
         }
         System.out.println();
+    }
+
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате X Y");
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+        }
+        while (!isCellValid(x, y));
+        map[x][y] = DOT_X;// не забыть
+    }
+
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
+            return false;
+        }
+        return map[x][y] == DOT_EMPTY;
     }
 }
